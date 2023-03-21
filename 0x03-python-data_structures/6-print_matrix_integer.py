dfc93ff5b802b7fdbd2af6acd8aeb39d6493b9b6
root <mwerumaryann@gmail.com>
2023-03-21 05:45:20 -0700

    Changes made in the file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in matrix]))
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
