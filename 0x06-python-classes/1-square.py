f9e9c468bc789211100f68f68aa7daa4b3f3ddbb
Maryann-Mweru_Wambui-Programmer <mwerumaryann@gmail.com>
2023-03-29 01:29:45 +0000

    Adjustments made on the file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-"""Class definition Square"""
+"""Class definition Square."""
 
 
 class Square:
-    """Class atrributes"""
+    """Class atrributes."""
 
-    def __init__ (self, size):
+    def __init__(self, size):
         """A private instance of the class.
         Args:
-        self(int):the size of the square.
+        self (int):the size of the square.
         """
         self.__size = size
