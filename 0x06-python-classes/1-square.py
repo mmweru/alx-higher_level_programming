b68fcc898bcc90f4dd3f8420e1f14c12906ead8f
Maryann-Mweru_Wambui-Programmer <mwerumaryann@gmail.com>
2023-03-29 01:08:15 +0000

    A class with a private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Class definition Square"""
+
+
+class Square:
+    """Class atrributes"""
+
+    def __init__ (self, size):
+        """A private instance of the class.
+        Args:
+        self:the size of the square.
+        """
+        self.__size = size
