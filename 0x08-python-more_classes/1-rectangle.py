9b57f2a84c470ee05d72bd634e091eb6ca53cb6b
Maryann Wambui <mwerumaryann@gmail.com>
2023-04-08 04:17:58 -0700

    Adjustments made on the file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """The class Rectangle."""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         """Instantiating attributes.
 
         Args:
