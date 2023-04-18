8c0c05fcb234adda99ec15fc5e4a54466b4fe966
Maryann Wambui <mwerumaryann@gmail.com>
2023-04-18 04:59:03 -0700

    A function that imports modules and uses add to sum up two integers

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Shebang line at the introductory part of the code"""
+
+
+from add_0 import add
+"""Importation of the add script"""
+
+a = 1
+b = 2
+"""Declaration of the two variables a: Int and b: Int"""
+
+print("{} + {} = ".format(a, b), add(a, b))
