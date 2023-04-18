37772809a53b93e4abc8edbfc1b9f2bd739d5bf0
Maryann Wambui <mwerumaryann@gmail.com>
2023-04-18 05:16:12 -0700

    Adjustments made on the file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,12 @@
 """Shebang line at the introductory part of the code"""
 
 
-from add_0 import add
-"""Importation of the add script"""
+if __name__ == "__main__":
+    from add_0 import add
+    """Importation of the add script"""
 
-a = 1
-b = 2
-"""Declaration of the two variables a: Int and b: Int"""
+    a = 1
+    b = 2
+    """Declaration of the two variables a: Int and b: Int"""
 
-print("{} + {} = ".format(a, b), add(a, b))
+    print("{} + {} = ".format(a, b), add(a, b))
