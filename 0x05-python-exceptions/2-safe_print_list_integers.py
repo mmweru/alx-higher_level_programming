beb54362d79a5c55dce405ddc6da9910a604b861
Maryann Wambui <mwerumaryann@gmail.com>
2023-04-25 03:35:39 -0700

    User of try/exception blocks to output integers in a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     s = 0
-    for value in my_list[0: x]:
+    for value in range(x):
         try:
-            print("{:d}".format(value), end="")
+            print("{:d}".format(my_list[value]), end="")
             s += 1
 
         except (ValueError, TypeError):
