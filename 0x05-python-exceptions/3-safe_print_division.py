6353b5ea471f74483b68f6a8e87e7bb5dbd7db10
Maryann Wambui <mwerumaryann@gmail.com>
2023-04-26 01:02:21 -0700

    Changes made in the format section of the function

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        if b == 0 or a == 0:
+        if b == 0:
             print("Inside result: {}".format(None))
         else:
             print("Inside result: {}".format(a / b))
