a961b6813d90ea28911251d3ccbdefb9d86cb702
Maryann Wambui <mwerumaryann@gmail.com>
2023-04-25 23:01:38 -0700

    Checks for Zerodivision error

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+    finally:
+        if b != 0:
+            print("Inside result: {}".format(div))
+        else:
+            print("Inside result: {}".format(div))
