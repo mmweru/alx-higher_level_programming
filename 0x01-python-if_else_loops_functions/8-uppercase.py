965a4ace341256a4f041c3d9cfefbf498962e922
Maryann-Mweru_Wambui-Programmer <mwerumaryann@gmail.com>
2023-07-14 21:55:56 +0000

    Updates made in the function to convert lowercase to uppercase characters

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str = ""
-    for i in str:
-        if (ord(i) >= 97) and ord(i) <= 122:
-            new_str += chr(ord(i) - 32)
-        else:
-            new_str += i
-    print(new_str)
+    """Function to convert uppercase characters to lowercase"""
+
+    new_string = ""
+    for element in str:
+        element = ord(element)
+        if (element >= 97) and element <= 122:
+            element -= 32
+        new_string += chr(element)
+    print(new_string)
