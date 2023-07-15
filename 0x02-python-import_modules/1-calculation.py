3a324a747cc87d5f9cadae058707325558c1f69a
Maryann-Mweru_Wambui-Programmer <mwerumaryann@gmail.com>
2023-07-15 00:30:09 +0000

    Adjustments made on the file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Importation of the three functions."""
-
     from calculator_1 import div, mul, add, sub
-    a = 10
-    b = 5
-
-    print("{} = {}".format(add({a} + {b}), add(a, b)))
-    print("{} = {}".format(sub({a} - {b}), sub(a, b)))
-    print("{} = {}".format(mul({a} * {b}), mul(a, b)))
-    print("{} = {}".format(div({a} / {b}), div(a, b)))
+a = 10
+b = 5
+print("{} = {}".format(add({a} + {b}), add(a, b)))
+print("{} = {}".format(sub({a} - {b}), sub(a, b)))
+print("{} = {}".format(mul({a} * {b}), mul(a, b)))
+print("{} = {}".format(div({a} / {b}), div(a, b)))
